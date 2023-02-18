933322b44d8ca187d762507ca39c6a6d8553eeb9
bofenghuang <bofenghuang7@gmail.com>
2023-02-18 17:34:32 +0100

    Update summ example

--- examples/pytorch/summarization/scripts/aggregate_text_soda.py
+++ examples/pytorch/summarization/scripts/aggregate_text_soda.py
@@ -7,50 +7,52 @@ import pandas as pd
 from tqdm import tqdm
 
 
-csv_path = "data/allenai_soda-text/test.tsv"
+for split in ["validation", "train", "test"]:
 
-df = pd.read_csv(csv_path, sep="\t")
-print(df.head())
+    csv_path = f"data/allenai_soda-text-fr/{split}.tsv"
 
-# debug
-# df = df[:10]
+    df = pd.read_csv(csv_path, sep="\t")
+    print(df.head())
 
-data = []
-for example_idx, example_df in tqdm(df.groupby("idx")):
-    # print(example_idx)
-    # print(example_df)
+    # debug
+    # df = df[:10]
 
-    summary = " ".join(example_df[example_df["type"] == "summary"]["sentence"])
-    # print(summary)
+    data = []
+    for example_idx, example_df in tqdm(df.groupby("idx")):
+        # print(example_idx)
+        # print(example_df)
 
-    example_dialog_df = example_df[example_df["type"] == "dialogue"]
+        summary = " ".join(example_df[example_df["type"] == "summary"]["sentence"])
+        # print(summary)
 
-    dialogs = []
-    for spk_turn_idx, spk_turn_df in example_dialog_df.groupby("spk_turn_idx"):
-        speaker_name = spk_turn_df["speaker"].unique()[0]
-        # print(speaker_name)
+        example_dialog_df = example_df[example_df["type"] == "dialogue"]
 
-        new_spk_turn = speaker_name + ": " + " ".join(spk_turn_df["sentence"])
-        # print(new_spk_turn)
-        dialogs.append(new_spk_turn)
+        dialogs = []
+        for spk_turn_idx, spk_turn_df in example_dialog_df.groupby("spk_turn_idx"):
+            speaker_name = spk_turn_df["speaker"].unique()[0]
+            # print(speaker_name)
 
-    dialog_text = "\n".join(dialogs)
+            new_spk_turn = speaker_name + ": " + " ".join(spk_turn_df["sentence"])
+            # print(new_spk_turn)
+            dialogs.append(new_spk_turn)
 
-    data.append(
-        {
-            "dialogue": dialog_text,
-            "summary": summary,
-        }
-    )
+        dialog_text = "\n".join(dialogs)
 
-new_df = pd.DataFrame(data)
-print(new_df.shape)
-print(new_df)
+        data.append(
+            {
+                "dialogue_fr": dialog_text,
+                "summary_fr": summary,
+            }
+        )
 
-output_dir = "data/allenai_soda-fr"
-# output_dir = "data/tmp"
+    new_df = pd.DataFrame(data)
+    print(new_df.shape)
+    print(new_df)
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    output_dir = "data/allenai_soda-fr"
+    # output_dir = "data/tmp"
 
-new_df.to_csv(f"{output_dir}/test.tsv", index=False, sep="\t")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    new_df.to_csv(f"{output_dir}/{split}.tsv", index=False, sep="\t")
