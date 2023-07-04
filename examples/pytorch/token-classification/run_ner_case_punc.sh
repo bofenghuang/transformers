5a98ae21e82616ca0ea16ed2fe62debcf173b2c4
bofenghuang <bofenghuang7@gmail.com>
2023-07-04 11:39:40 +0200

    update punctuation

--- examples/pytorch/token-classification/run_ner_case_punc.sh
+++ examples/pytorch/token-classification/run_ner_case_punc.sh
@@ -12,18 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export TRANSFORMERS_CACHE="/projects/bhuang/.cache/huggingface/transformers"
+export HF_DATASETS_CACHE="/projects/bhuang/.cache/huggingface/datasets"
+
 # export WANDB_MODE=disabled
 # export WANDB_DISABLED=true
 # export WANDB_API_KEY=YOUR_WANDB_API_KEY
 # export WANDB_ENTITY=YOUR_WANDB_ENTITY
-export WANDB_PROJECT=hf-punc
-# export WANDB_NAME=wav2vec2-xls-r-1b-ft-medLR-ep30
+export WANDB_PROJECT="case_punctuation"
 
 # https://github.com/pytorch/audio/issues/1021#issuecomment-726915239
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS="1"
 
-# export CUDA_VISIBLE_DEVICES=5,4,3,2
-export CUDA_VISIBLE_DEVICES=3
+export TOKENIZERS_PARALLELISM="false"
+
+# export CUDA_VISIBLE_DEVICES="0,1,2,5"
+export CUDA_VISIBLE_DEVICES=""
 
 
 	# --model_name_or_path "camembert-base" \
@@ -76,76 +80,87 @@ export CUDA_VISIBLE_DEVICES=3
 # 	--do_train \
 # 	--do_eval
 
+# todo
+# train strategy "all"
+
+	# --preprocess_label_strategy '{"case": "first", "punc": "last"}' \
+	# --preprocess_label_replacers '{"case": {"OTHER": "LOWER"}, "punc": {"EXCLAMATION": "PERIOD", "COLON": "COMMA"}}' \
+	# --group_by_length \
+	# --fp16 \
+
 python3 run_ner_case_punc.py \
 	--task_config '["case", "punc"]' \
-	--train_data_dir "/projects/bhuang/corpus/text/flaubert/raw/multilingual_plus/data/train" \
-	--validation_data_dir "/projects/bhuang/corpus/text/flaubert/raw/fr_europarl/data/validation" \
-	--test_data_dir "/projects/bhuang/corpus/text/flaubert/raw/fr_europarl/data/test" \
+	--train_data_dir "/projects/bhuang/corpus/text/case_punctuation/final/train/fr_tedtalks" \
+	--validation_data_dir "/projects/bhuang/corpus/text/case_punctuation/final/validation/fr_tedtalks" \
+	--test_data_dir "/projects/bhuang/corpus/text/case_punctuation/final/test/fr_tedtalks" \
 	--text_column_name "word" \
 	--label_column_name "label" \
 	--preprocess_stride "100" \
 	--preprocess_label_strategy '{"case": "first", "punc": "last"}' \
-	--preprocess_label_replacers '{"case": {"OTHER": "LOWER"}, "punc": {"EXCLAMATION": "PERIOD", "COLON": "COMMA"}}' \
-	--group_by_length \
+	--preprocess_label_replacers '{"case": {"OTHER": "CAPITALIZE"}}' \
 	--preprocessing_num_workers "16" \
 	--dataloader_num_workers "1" \
 	--model_name_or_path "xlm-roberta-base" \
-	--output_dir "outputs/recasepunc-multilingual_plus/xlm-roberta-base_ft" \
+	--output_dir "outputs_2023/casepunc_fr/xlm_roberta_base_ft" \
+	--run_name "xlm_roberta_base_ft" \
 	--overwrite_output_dir \
-	--num_train_epochs "5" \
-	--per_device_train_batch_size "64" \
-	--per_device_eval_batch_size "64" \
+	--per_device_train_batch_size "1" \
+	--per_device_eval_batch_size "1" \
 	--gradient_accumulation_steps "1" \
-	--gradient_checkpointing \
+	--num_train_epochs "3" \
 	--learning_rate "5e-5" \
 	--warmup_ratio "0.1" \
+	--lr_scheduler_type "cosine" \
 	--weight_decay "0.01" \
-	--logging_steps "100" \
-	--evaluation_strategy "steps" \
-	--eval_steps "500" \
+	--gradient_checkpointing \
+	--logging_steps "10" \
+	--logging_first_step \
+	--save_strategy "steps" \
 	--save_steps "500" \
 	--save_total_limit "3" \
+	--evaluation_strategy "steps" \
+	--eval_steps "500" \
 	--load_best_model_at_end \
-	--fp16 \
+	--report_to "tensorboard" "wandb" \
 	--do_train \
 	--do_eval \
 	--do_predict
 
 
-python3 run_ner_case_punc.py \
-	--task_config '["case", "punc"]' \
-	--train_data_dir "/projects/bhuang/corpus/text/flaubert/raw/multilingual_plus/data/train" \
-	--validation_data_dir "/projects/bhuang/corpus/text/flaubert/raw/fr_europarl/data/validation" \
-	--test_data_dir "/projects/bhuang/corpus/text/flaubert/raw/fr_europarl/data/test" \
-	--text_column_name "word" \
-	--label_column_name "label" \
-	--preprocess_stride "100" \
-	--preprocess_label_strategy '{"case": "first", "punc": "last"}' \
-	--preprocess_label_replacers '{"case": {"OTHER": "LOWER"}, "punc": {"EXCLAMATION": "PERIOD", "COLON": "COMMA"}}' \
-	--group_by_length \
-	--preprocessing_num_workers "16" \
-	--dataloader_num_workers "1" \
-	--model_name_or_path "xlm-roberta-large" \
-	--output_dir "outputs/recasepunc-multilingual_plus/xlm-roberta-large_ft" \
-	--overwrite_output_dir \
-	--num_train_epochs "5" \
-	--per_device_train_batch_size "64" \
-	--per_device_eval_batch_size "64" \
-	--gradient_accumulation_steps "1" \
-	--gradient_checkpointing \
-	--learning_rate "5e-5" \
-	--warmup_ratio "0.1" \
-	--weight_decay "0.01" \
-	--logging_steps "100" \
-	--evaluation_strategy "steps" \
-	--eval_steps "500" \
-	--save_steps "500" \
-	--save_total_limit "3" \
-	--load_best_model_at_end \
-	--fp16 \
-	--do_train \
-	--do_eval \
-	--do_predict
+# python3 run_ner_case_punc.py \
+# 	--task_config '["case", "punc"]' \
+# 	--train_data_dir "/projects/bhuang/corpus/text/flaubert/raw/multilingual_plus/data/train" \
+# 	--validation_data_dir "/projects/bhuang/corpus/text/flaubert/raw/fr_europarl/data/validation" \
+# 	--test_data_dir "/projects/bhuang/corpus/text/flaubert/raw/fr_europarl/data/test" \
+# 	--text_column_name "word" \
+# 	--label_column_name "label" \
+# 	--preprocess_stride "100" \
+# 	--preprocess_label_strategy '{"case": "first", "punc": "last"}' \
+# 	--preprocess_label_replacers '{"case": {"OTHER": "LOWER"}, "punc": {"EXCLAMATION": "PERIOD", "COLON": "COMMA"}}' \
+# 	--group_by_length \
+# 	--preprocessing_num_workers "16" \
+# 	--dataloader_num_workers "1" \
+# 	--model_name_or_path "xlm-roberta-large" \
+# 	--output_dir "outputs/recasepunc-multilingual_plus/xlm-roberta-large_ft" \
+# 	--overwrite_output_dir \
+# 	--num_train_epochs "5" \
+# 	--per_device_train_batch_size "64" \
+# 	--per_device_eval_batch_size "64" \
+# 	--gradient_accumulation_steps "1" \
+# 	--gradient_checkpointing \
+# 	--learning_rate "5e-5" \
+# 	--warmup_ratio "0.1" \
+# 	--weight_decay "0.01" \
+# 	--logging_steps "100" \
+# 	--evaluation_strategy "steps" \
+# 	--eval_steps "500" \
+# 	--save_steps "500" \
+# 	--save_total_limit "3" \
+# 	--load_best_model_at_end \
+# 	--fp16 \
+# 	--do_train \
+# 	--do_eval \
+# 	--do_predict
 
 
 # python3 run_ner_case_punc.py \
--- examples/pytorch/token-classification/utils/callbacks_ner_case_punc.py
+++ examples/pytorch/token-classification/utils/callbacks_ner_case_punc.py
@@ -2,8 +2,71 @@
 # coding=utf-8
 # Copyright 2023  Bofeng Huang
 
+import numpy as np
+from typing import List, Optional, Union
 
 from transformers.integrations import WandbCallback
+
+def gather_pre_entities(
+        tokenizer,
+        sentence: str,
+        input_ids: np.ndarray,
+        attention_mask: np.ndarray,
+        case_scores: np.ndarray,
+        punc_scores: np.ndarray,
+        offset_mapping: np.ndarray,
+    ) -> List[dict]:
+        pre_entities = []
+
+        last_word = None
+        for idx, (input_id_, attention_mask_) in enumerate(input_ids, attention_mask):
+
+            if attention_mask_ == 0:
+                break
+
+            word = tokenizer.convert_ids_to_tokens(int(input_id_))
+            # normally there won't be None here
+            if offset_mapping is not None:
+                start_ind, end_ind = offset_mapping[idx]
+                word_ref = sentence[start_ind:end_ind]
+                # todo: This is a fallback heuristic. This will fail most likely on any kind of text + punctuation mixtures that will be considered "words". Non word aware models cannot do better than this unfortunately.
+                # is_subword = start_ind > 0 and " " not in sentence[start_ind - 1 : start_ind + 1]
+                # todo: "l' ue" gives unexpected token (l'", "▁", "ue") (not "_") whose offset mapping is wrong
+                # todo: "ue" doesn't apply to HF rules, and we may need prediction of the token "▁"
+                is_subword = start_ind > 0 and " " not in sentence[start_ind - 1 : start_ind + 1] if last_word != "▁" else True
+                # print(f"-{word}-{word_ref}-{start_ind}-{end_ind}-{sentence[start_ind - 1 : start_ind + 1]}-{is_subword}")
+
+                if int(input_id_) == tokenizer.unk_token_id:
+                    word = word_ref
+                    # why set to False
+                    # is_subword = False
+            else:
+                start_ind = None
+                end_ind = None
+                is_subword = False
+
+            pre_entity = {
+                "word": word,
+                "case_scores": case_scores[idx],
+                "punc_scores": punc_scores[idx],
+                "start": start_ind,
+                "end": end_ind,
+                "index": idx,
+                "is_subword": is_subword,
+            }
+            pre_entities.append(pre_entity)
+
+            last_word = word
+
+        # debug
+        # i = -1
+        # for e in pre_entities:
+        #     if not e["is_subword"]:
+        #         i += 1
+        #     print(i, e)
+        # quit()
+
+        return pre_entities
 
 
 class WandbProgressResultsCallback(WandbCallback):
@@ -17,8 +80,24 @@ class WandbProgressResultsCallback(WandbCallback):
         super().on_log(args, state, control, model, logs)
 
         predictions = self.trainer.predict(self.sample_dataset)
+        case_logits, punc_logits = predictions.predictions
 
-        
+        case_maxes = np.max(case_logits, axis=-1, keepdims=True)
+        case_shifted_exp = np.exp(case_logits - case_maxes)
+        case_scores = case_shifted_exp / case_shifted_exp.sum(axis=-1, keepdims=True)
+
+        punc_maxes = np.max(punc_logits, axis=-1, keepdims=True)
+        punc_shifted_exp = np.exp(punc_logits - punc_maxes)
+        punc_scores = punc_shifted_exp / punc_shifted_exp.sum(axis=-1, keepdims=True)
+
+        for example, case_scores_, punc_scores_ in zip(self.sample_dataset, case_scores, punc_scores):
+            sentence = example["text"]
+            input_ids = example["input_ids"]
+            attention_mask = example["attention_mask"]
+            
+            
+
+
 
         predictions = decode_predictions(self.trainer, predictions)
         measures_df = compute_measures(predictions, self.records_df["sentence"].tolist())
